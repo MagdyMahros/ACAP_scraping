e379d4f93c77402029767813447c4cf49863b4af
MagdyMahros <majdy090@gmail.com>
2020-11-17 12:44:39 +0800

    courses title language level code and faculty

---
+++ courses/ACAP_courses_script.py
@@ -0,0 +1,100 @@
+"""Description:
+    * author: Magdy Abdelkader
+    * company: Fresh Futures/Seeka Technology
+    * position: IT Intern
+    * date: 03-11-20
+    * description:This script extracts the corresponding undergraduate courses details and tabulate it.
+"""
+
+import csv
+import re
+import time
+from pathlib import Path
+from selenium import webdriver
+import bs4 as bs4
+import os
+import copy
+from CustomMethods import TemplateData
+from CustomMethods import DurationConverter as dura
+
+option = webdriver.ChromeOptions()
+option.add_argument(" - incognito")
+option.add_argument("headless")
+exec_path = Path(os.getcwd().replace('\\', '/'))
+exec_path = exec_path.parent.__str__() + '/Libraries/Google/v86/chromedriver.exe'
+browser = webdriver.Chrome(executable_path=exec_path, options=option)
+
+# read the url from each file into a list
+course_links_file_path = Path(os.getcwd().replace('\\', '/'))
+course_links_file_path = course_links_file_path.__str__() + '/ACAP_courses_links.txt'
+course_links_file = open(course_links_file_path, 'r')
+
+# the csv file we'll be saving the courses to
+csv_file_path = Path(os.getcwd().replace('\\', '/'))
+csv_file = csv_file_path.__str__() + '/ACAP_courses.csv'
+
+course_data = {'Level_Code': '', 'University': 'Australian College of Applied Psychology', 'City': '',
+               'Country': 'Australia', 'Course': '', 'Int_Fees': '', 'Local_Fees': '', 'Currency': 'AUD',
+               'Currency_Time': 'year','Duration': '', 'Duration_Time': '', 'Full_Time': '', 'Part_Time': '',
+               'Prerequisite_1': '', 'Prerequisite_2': 'IELTS', 'Prerequisite_3': '', 'Prerequisite_1_grade': '',
+               'Prerequisite_2_grade': '6.5', 'Prerequisite_3_grade': '', 'Website': '', 'Course_Lang': '',
+               'Availability': '', 'Description': '','Career_Outcomes': '', 'Online': '', 'Offline': '', 'Distance': '',
+               'Face_to_Face': '', 'Blended': '', 'Remarks': ''}
+
+possible_cities = {'rockhampton': 'Rockhampton', 'cairns': 'Cairns', 'bundaberg': 'Bundaberg',
+                   'townsville': 'Townsville', 'canberra': 'Canberra', 'paddington': 'Paddington',
+                   'online': 'Online', 'gladstone': 'Gladstone', 'mackay': 'Mackay', 'mixed': 'Online',
+                   'yeppoon': 'Yeppoon', 'brisbane': 'Brisbane', 'sydney': 'Sydney', 'queensland': 'Queensland',
+                   'melbourne': 'Melbourne', 'albany': 'Albany', 'perth': 'Perth', 'adelaide': 'Adelaide',
+                   'noosa': 'Noosa', 'emerald': 'Emerald', 'hawthorn': 'Hawthorn', 'wantirna': 'Wantirna',
+                   'prahran': 'Prahran', 'kensington': 'Kensington'}
+
+possible_languages = {'Japanese': 'Japanese', 'French': 'French', 'Italian': 'Italian', 'Korean': 'Korean',
+                      'Indonesian': 'Indonesian', 'Chinese': 'Chinese', 'Spanish': 'Spanish'}
+
+course_data_all = []
+level_key = TemplateData.level_key  # dictionary of course levels
+faculty_key = TemplateData.faculty_key  # dictionary of course levels
+
+# GET EACH COURSE LINK
+for each_url in course_links_file:
+    actual_cities = []
+    remarks_list = []
+    browser.get(each_url)
+    pure_url = each_url.strip()
+    each_url = browser.page_source
+
+    soup = bs4.BeautifulSoup(each_url, 'lxml')
+    time.sleep(1)
+
+    # SAVE COURSE URL
+    course_data['Website'] = pure_url
+
+    # SAVE COURSE TITLE
+    title_tag = soup.find('h1', class_='h2')
+    if title_tag:
+        course_data['Course'] = title_tag.get_text()
+        print('COURSE TITLE: ', course_data['Course'])
+
+
+        # DECIDE THE LEVEL CODE
+        for i in level_key:
+            for j in level_key[i]:
+                if j in course_data['Course']:
+                    course_data['Level_Code'] = i
+        print('COURSE LEVEL CODE: ', course_data['Level_Code'])
+
+        # DECIDE THE FACULTY
+        for i in faculty_key:
+            for j in faculty_key[i]:
+                if j.lower() in course_data['Course'].lower():
+                    course_data['Faculty'] = i
+        print('COURSE FACULTY: ', course_data['Faculty'])
+
+        # COURSE LANGUAGE
+        for language in possible_languages:
+            if language in course_data['Course']:
+                course_data['Course_Lang'] = language
+            else:
+                course_data['Course_Lang'] = 'English'
+        print('COURSE LANGUAGE: ', course_data['Course_Lang'])
